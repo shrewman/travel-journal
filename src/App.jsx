0dcd9eb4ce4c992dd3e017d68c0ea4e76d4ebd2e
feelswhiteman <feelswhiteman@gmail.com>
2024-01-11 15:45:14 +0200

    Project is already done

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+import data from "./data.js";
+import Header from "./components/Header";
+import Card from "./components/Card";
+
+function App() {
+    console.log(data);
+    return (
+        <>
+            <Header />
+            {data.map((place) => (
+                <Card key={place.id} place={place} />
+            ))}
+        </>
+    );
+}
+
+export default App;
